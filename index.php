55e616092e7df16adef33600bcc064668f206a5c
Nick Cheney <nicheney.9@gmail.com>
2022-01-17 22:14:59 -0500

    edited login sys

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT * FROM user_ WHERE username = :username";
+        $sql = "SELECT * FROM user WHERE username = :username";
         
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
